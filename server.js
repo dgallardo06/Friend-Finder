1ed222cccee27b25be931c813f89d210daf6948d
dgallardo06 <denise.gallardo1@gmail.com>
2018-03-05 23:17:45 -0500

    require routes js files

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
