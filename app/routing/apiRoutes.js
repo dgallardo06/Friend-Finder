812f5d82d1c3ed54d931343b1b52d10a5579eb3f
dgallardo06 <denise.gallardo1@gmail.com>
2018-03-07 23:59:08 -0500

    check scores and compare for best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require("../data/friends.js");
+var friendsData = require("../data/friends.js");
 
 module.exports = function (app){
 
@@ -7,8 +7,34 @@ module.exports = function (app){
 	});
 
 	app.post("/api/friends", function(req,res){
-		if (friendsData.length ){
-			
+		var newFriendScores = req.body.scores;
+		var scoresArray = [];
+		var friendCount = 0;
+		var bestMatch = 0;
+
+		for(var i=0; i<friendsData.length; i++){
+		  var scoresDiff = 0;
+		  //run through scores to compare friends
+		  for(var j=0; j<newFriendScores.length; j++){
+		    scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
+		  }
+
+		  scoresArray.push(scoresDiff);
+		}
+
+		//after all friends are compared, find best match
+		for(var i=0; i<scoresArray.length; i++){
+		  if(scoresArray[i] <= scoresArray[bestMatch]){
+		    bestMatch = i;
+		  }
+		}
+
+		//return bestMatch data
+		var bff = friendsData[bestMatch];
+		res.json(bff);
+
+		//pushes new submission into the friendsList array
+		friendsData.push(req.body);	
 		}
 	});
 
