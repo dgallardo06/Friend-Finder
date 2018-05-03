184c2bb3cb31ef0ae00b5191ec8ae12aebff3836
dgallardo06 <denise.gallardo1@gmail.com>
2018-05-02 22:38:06 -0400

    add photo links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
 	{
-		name: Shaq,
-		photo: ,
+		name: "Shaq",
+		photo: "https://tombuchheim.files.wordpress.com/2011/06/shaq.jpg",
 		scores: [
 			"1",
 			"4",
@@ -16,8 +16,8 @@ var friends = [
 		]
 	},
 	{
-		name: Jim,
-		photo: ,
+		name: "Jim",
+		photo: "https://en.wikipedia.org/wiki/Jim_Halpert#/media/File:Jim-halpert.jpg",
 		scores: [
 			"4",
 			"4",
@@ -32,8 +32,8 @@ var friends = [
 		]
 	},
 	{
-		name: Ana,
-		photo: ,
+		name: "Anna",
+		photo: "https://vignette.wikia.nocookie.net/disney/images/9/97/Anna_Render2.png/revision/latest?cb=20160211030147",
 		scores: [
 			"3",
 			"4",
